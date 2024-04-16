2e30edafb59d155aec21c18cdcbdff465147f50a
1-mathis <157580769+1-mathis@users.noreply.github.com>
2024-04-16 15:48:13 +0200

    add controller

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Thread;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -13,6 +15,34 @@ class HomeController extends AbstractController
     {
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+        ]);
+    }
+
+    #[Route('/home', name: 'app_home')]
+    public function affichage(EntityManagerInterface $entityManager): Response
+    {
+
+        $threadRepository = $entityManager->getRepository(Thread::class);
+
+        $threads = $threadRepository->findAll();
+
+        return $this->render('home/index.html.twig', [
+            'controller_name' => 'HomeController',
+            'threads' => $threads
+        ]);
+    }
+
+    #[Route('/home/thread/{id}', name: 'app_thread_details')]
+    public function details($id, EntityManagerInterface $entityManager): Response
+    {
+
+        $threadRepository = $entityManager->getRepository(Thread::class);
+
+        $thread = $threadRepository->find($id);
+
+        return $this->render('thread/details.html.twig', [
+            'controller_name' => 'HomeController',
+            'thread' => $thread
         ]);
     }
 }
--- src/Controller/ThreadController.php
+++ src/Controller/ThreadController.php
@@ -2,17 +2,50 @@
 
 namespace App\Controller;
 
+use App\Entity\Thread;
+use App\Form\ThreadFormType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class ThreadController extends AbstractController
 {
+    // #[Route('/thread', name: 'app_thread')]
+    // public function index(Request $request): Response
+    // {
+    //     $thread = new Thread();
+    //     $form = $this->createForm(ThreadFormType::class, $thread);
+
+    //     $form->handleRequest($request);
+    //     if ($form->isSubmitted() && $form->isValid()) {
+
+    //         return $this->redirectToRoute('home/index.html.twig');
+    //     }
+
+    //     return $this->render('thread/index.html.twig', [
+    //         'form' => $form->createView(),
+    //     ]);
+    // }
+
     #[Route('/thread', name: 'app_thread')]
-    public function index(): Response
+    public function index(EntityManagerInterface $entityManager, Request $request): Response
     {
+        $thread = new Thread();
+        $form = $this->createForm(ThreadFormType::class, $thread);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $thread = $form->getData();
+
+            $entityManager->persist($thread);
+            $entityManager->flush();
+        }
+
         return $this->render('thread/index.html.twig', [
-            'controller_name' => 'ThreadController',
+            'form' => $form
         ]);
     }
 }
