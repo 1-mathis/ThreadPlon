04cd04500bc7ca910c72e25ecf26aa01e77b8d7e
1-mathis <157580769+1-mathis@users.noreply.github.com>
2024-04-16 16:52:08 +0200

    fonctionnel sans form edit thread

--- src/Controller/ThreadController.php
+++ src/Controller/ThreadController.php
@@ -12,23 +12,6 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class ThreadController extends AbstractController
 {
-    // #[Route('/thread', name: 'app_thread')]
-    // public function index(Request $request): Response
-    // {
-    //     $thread = new Thread();
-    //     $form = $this->createForm(ThreadFormType::class, $thread);
-
-    //     $form->handleRequest($request);
-    //     if ($form->isSubmitted() && $form->isValid()) {
-
-    //         return $this->redirectToRoute('home/index.html.twig');
-    //     }
-
-    //     return $this->render('thread/index.html.twig', [
-    //         'form' => $form->createView(),
-    //     ]);
-    // }
-
     #[Route('/thread', name: 'app_thread')]
     public function index(EntityManagerInterface $entityManager, Request $request): Response
     {
